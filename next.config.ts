baf88b1fd8bae26d22c48b3097f04c0a7664ba8e
timchristoffer <timdaahl@gmail.com>
2024-11-18 13:23:37 +0100

    Edited next.config.ts to allow for static exports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
